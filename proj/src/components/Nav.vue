a01bdbcc42c0a67c635bd5b2f5bf47b03481b05d
mrbonur <40200429@esmad.ipp.pt>
2023-01-27 21:09:26 +0000

    remove last nav fix

--- proj/src/components/Nav.vue
+++ proj/src/components/Nav.vue
@@ -7,8 +7,7 @@ const authStore = useAuthStore();
 <template>
   <nav v-show="authStore.user" class="navbar navbar-expand navbar-dark">
     <div>
-      
-      <router-link to="/" class="nav-item nav-link"><img class="logo" src="../assets/ECOCAMPUS3.svg" alt=""></router-link>
+      <img class="logo" src="../assets/ECOCAMPUS3.svg" alt="">
     </div>
     <div class="navbar-nav">
         <router-link to="/" class="nav-item nav-link">Home</router-link>
