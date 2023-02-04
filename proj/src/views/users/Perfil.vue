77b700bf533a093ce0469912cf34097a0f19362d
mrbonur <40200429@esmad.ipp.pt>
2023-02-04 13:25:14 +0000

    data for perfil page

--- proj/src/views/users/Perfil.vue
+++ proj/src/views/users/Perfil.vue
@@ -3,65 +3,36 @@
 </script>
 
 <template>
-  	<div>
 		<div class="container">
-			<div class="topContainer">
-				<img
-					src="../../public/assets/user.png"
-					id="profile"
-					alt=""
-					width="100"
-					height="100"
-				/>
-				<!-- <h1>Olá {{ getLoggedUser.userName }}!</h1> -->
-			</div>
 			<div class="secondContainer">
-				<h4>Informações do Utilizador</h4>
+				<h4>Edite os seus dados!</h4>
 				<div class="divider"></div>
-				<button>Editar</button>
-
 				<div class="editContainer">
 					<div class="infoContainer nameContainer">
-						<span>Primeiro Nome</span>
-						<span class="value"></span>
+						<span>Primeiro Nome:</span>
+						<input class="value" type="text" name="firstName" id="firstName" />
 					</div>
 					<div class="infoContainer surnameContainer">
-						<span>Apelido</span>
-						<span class="value"></span>
+						<span>Apelido:</span>
+						<input class="value" type="text" name="lastName" id="lastName" />
 					</div>
-					<div class="infoContainer numberContainer">
-						<span>Número de aluno</span>
-						<span class="value"></span>
+					<div class="infoContainer surnameContainer">
+						<span>Nome de utilizador:</span>
+						<input class="value" type="text" name="username" id="username" />
 					</div>
-					<div class="infoContainer dobContainer">
-						<span>Data de Nascimento</span>
-						<span class="value"></span>
+					<div class="infoContainer surnameContainer">
+						<span>Palavra-passe:</span>
+						<input type="password" name="password" id="password" />
 					</div>
-					<div class="infoContainer emailContainer">
-						<span>Email</span>
-						<span class="value"></span>
-					</div>
-					<div class="infoContainer nifContainer">
-						<span>NIF</span>
-						<span class="value"></span>
-					</div>
+					
 				</div>
 			</div>
-
-			<div class="thirdContainer">
-				<h4>Editar Palavra-passe</h4>
-				<div class="divider"></div>
-				<div class="passContainer">
-					<form>
-						<label for="actualPass">Palavra-passe atual</label>
-						<input type="password" name="" id="" />
-					</form>
-				</div>
-			</div>
+			<button>Editar</button>
 		</div>
-	</div>
 </template>
 
 <style>
-
+.container {
+	background-color: white;
+}
 </style>
