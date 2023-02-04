41f0362ec8e0d6466484670710677c58ab76d468
mrbonur <40200429@esmad.ipp.pt>
2023-02-04 16:31:55 +0000

    questons, ocorrencies store start

---
+++ proj/src/stores/ocorrencies.store.js
@@ -0,0 +1,9 @@
+import { defineStore } from "pinia";
+
+export const useOcorrencieStore = defineStore('OcorrencieStore',{
+    state: {},
+    getters: {},
+    mutations: {},
+    actions: {},
+    
+})
---
+++ proj/src/stores/questions.store.js
@@ -0,0 +1,9 @@
+import { defineStore } from "pinia";
+
+export const useQuestionStore = defineStore('QuestionStore',{
+    state: {},
+    getters: {},
+    mutations: {},
+    actions: {},
+    
+})
