7c95a58f3f303c73a12d9100e828b77d882b78b1
mrbonur <40200429@esmad.ipp.pt>
2023-02-06 15:22:02 +0000

    activities store added

--- proj/src/stores/act.store.js
+++ proj/src/stores/act.store.js
@@ -1,9 +1,46 @@
 import { defineStore } from "pinia";
+import { useAuthStore } from "./auth.store";
+const authStore = useAuthStore();
 
 export const useActStore = defineStore('ActStore',{
-    state: {},
-    getters: {},
-    mutations: {},
+    state: {
+        arrayActivities: [
+			{
+				id: 0,
+				titleAct: 'Atividade 1',
+				descriptionAct: 'Limpar as ruas',
+			},
+            {
+				id: 1,
+				titleAct: 'Atividade 2',
+				descriptionAct: 'Limpar as florestas    ',
+			},
+            {
+				id: 2,
+				titleAct: 'Atividade 3',
+				descriptionAct: 'Fazer a reciclagem',
+			},
+            {
+				id: 3,
+				titleAct: 'Atividade 4',
+				descriptionAct: 'Poster Eco-Códgio',
+			},
+		],
+    },
+    getters: {
+        getActivitiesByID: (state) => (id) =>
+			state.getActivitiesByID.filter((activities) => activities.id == id),
+        getLoggedUser: (state) => state.loggedUser,
+
+    },
+    mutations: {
+        SET_LOGGED_USER(state, payload) {
+			state.loggedUser = state.users.find(
+				(user) => user.email === payload
+			);
+			localStorage.loggedUser = JSON.stringify(state.loggedUser);
+		},
+    },
     actions: {},
     
 })
